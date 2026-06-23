2b6c3b7c0f499de77137180bf5a6a94c1c5b8ce2
rafikmoha <m63866157@gmail.com>
2026-06-23 12:22:52 +0200

    feat: add dynamic dot loading animation to Meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useHttp from "../hooks/useHttp.js";
 import MealItem from "./MealItem.jsx";
 import Error from "./Error.jsx";
@@ -5,14 +6,36 @@ import Error from "./Error.jsx";
 const requestConfig = {};
 
 export default function Meals() {
+  const [dots, setDots] = useState(".");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) => (prevDots.length < 3 ? prevDots + "." : "."));
+    }, 400);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const {
     data: loadedMeals,
     isLoading,
     error,
-  } = useHttp("https://food-ordering-app-1-hh7u.onrender.com/meals", requestConfig, []);
+  } = useHttp(
+    "https://food-ordering-app-1-hh7u.onrender.com/meals",
+    requestConfig,
+    [],
+  );
 
   if (isLoading) {
-    return <p className="center">Fetching meals...</p>;
+    return (
+      <p className="center">
+        Fetching meals
+        <span
+          style={{ display: "inline-block", width: "20px", textAlign: "left" }}
+        >
+          {dots}
+        </span>
+      </p>
+    );
   }
 
   if (error) {
@@ -36,4 +59,3 @@ export default function Meals() {
     </ul>
   );
 }
-
